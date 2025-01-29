76adb81b14d8f952349a7e7ad2e03041b60891f0
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-01-29 12:49:44 +0100

    Add diff with similarity

--- src/Diff.elm
+++ src/Diff.elm
@@ -1,4 +1,8 @@
-module Diff exposing (Change(..), diff, diffLines)
+module Diff exposing
+    ( Change(..)
+    , diff, diffLines
+    , diffWith
+    )
 
 {-| Compares two list and returns how they have changed.
 Each function internally uses Wu's [O(NP) algorithm](http://myerslab.mpi-cbg.de/wp-content/uploads/2014/06/np_diff.pdf).
@@ -13,6 +17,8 @@ Each function internally uses Wu's [O(NP) algorithm](http://myerslab.mpi-cbg.de/
 
 @docs diff, diffLines
 
+@docs diffWith
+
 -}
 
 import Array exposing (Array)
@@ -20,9 +26,10 @@ import Array exposing (Array)
 
 {-| This describes how each line has changed and also contains its value.
 -}
-type Change a
+type Change similar a
     = Added a
     | Removed a
+    | Similar a a similar
     | NoChange a
 
 
@@ -37,7 +44,7 @@ type BugReport
     | UnexpectedPath ( Int, Int ) (List ( Int, Int ))
 
 
-{-| Compares two text.
+{-| Compares two text. Considers two lines similar if they are equivalent ignoring spaces.
 
 Giving the following text
 
@@ -64,19 +71,40 @@ results in
 .
 
 -}
-diffLines : String -> String -> List (Change String)
+diffLines : String -> String -> List (Change () String)
 diffLines a b =
-    diff (String.lines a) (String.lines b)
+    diffWith
+        (\l r ->
+            if String.replace " " "" (String.trim l) == String.replace " " "" (String.trim r) then
+                Just ()
+
+            else
+                Nothing
+        )
+        (String.lines a)
+        (String.lines b)
 
 
 {-| Compares general lists.
 
-    diff [1, 3] [2, 3] == [Removed 1, Added 2, NoChange 3] -- True
+    diff [ 1, 3 ] [ 2, 3 ]
+    --> [ Removed 1, Added 2, NoChange 3 ]
 
 -}
-diff : List a -> List a -> List (Change a)
-diff a b =
-    case testDiff a b of
+diff : List a -> List a -> List (Change Never a)
+diff =
+    diffWith (\_ _ -> Nothing)
+
+
+{-| Compares general lists. Allows specifying when two elements are similar.
+
+    diff (\l r -> if abs (l - r) < 1 then Just () else Nothing) [ 1, 3 ] [ 2, 3 ]
+    --> [ Similar 1 2 (), NoChange 3 ]
+
+-}
+diffWith : (a -> a -> Maybe similar) -> List a -> List a -> List (Change similar a)
+diffWith areSimilar a b =
+    case testDiff areSimilar a b of
         Ok changes ->
             changes
 
@@ -84,73 +112,92 @@ diff a b =
             []
 
 
-{-| Test the algolithm itself.
+{-| Test the algorithm itself.
 If it returns Err, it should be a bug.
 -}
-testDiff : List a -> List a -> Result BugReport (List (Change a))
-testDiff a b =
+testDiff : (a -> a -> Maybe similar) -> List a -> List a -> Result BugReport (List (Change similar a))
+testDiff areSimilar a b =
     let
+        arrA : Array a
         arrA =
             Array.fromList a
 
+        arrB : Array a
         arrB =
             Array.fromList b
 
+        m : Int
         m =
             Array.length arrA
 
+        n : Int
         n =
             Array.length arrB
 
         -- Elm's Array doesn't allow null element,
         -- so we'll use shifted index to access source.
+        getA : Int -> Maybe a
         getA =
             \x -> Array.get (x - 1) arrA
 
+        getB : Int -> Maybe a
         getB =
             \y -> Array.get (y - 1) arrB
 
+        path : List ( Int, Int )
         path =
-            -- Is there any case ond is needed?
-            -- ond getA getB m n
-            onp getA getB m n
+            onp areSimilar getA getB m n
     in
-    makeChanges getA getB path
+    makeChanges areSimilar getA getB path
 
 
 makeChanges :
-    (Int -> Maybe a)
+    (a -> a -> Maybe similar)
+    -> (Int -> Maybe a)
     -> (Int -> Maybe a)
     -> List ( Int, Int )
-    -> Result BugReport (List (Change a))
-makeChanges getA getB path =
+    -> Result BugReport (List (Change similar a))
+makeChanges areSimilar getA getB path =
     case path of
         [] ->
             Ok []
 
         latest :: tail ->
-            makeChangesHelp [] getA getB latest tail
+            makeChangesHelp areSimilar [] getA getB latest tail
 
 
 makeChangesHelp :
-    List (Change a)
+    (a -> a -> Maybe similar)
+    -> List (Change similar a)
     -> (Int -> Maybe a)
     -> (Int -> Maybe a)
     -> ( Int, Int )
     -> List ( Int, Int )
-    -> Result BugReport (List (Change a))
-makeChangesHelp changes getA getB ( x, y ) path =
+    -> Result BugReport (List (Change similar a))
+makeChangesHelp areSimilar changes getA getB ( x, y ) path =
     case path of
         [] ->
             Ok changes
 
         ( prevX, prevY ) :: tail ->
             let
+                change : Result BugReport (Change similar a)
                 change =
                     if x - 1 == prevX && y - 1 == prevY then
                         case getA x of
                             Just a ->
-                                Ok (NoChange a)
+                                case getB y of
+                                    Nothing ->
+                                        Err (CannotGetB y)
+
+                                    Just b ->
+                                        case areSimilar a b of
+                                            Just s ->
+                                                Ok (Similar a b s)
+
+                                            Nothing ->
+                                                --  This implies a == b by construction. I hope.
+                                                Ok (NoChange a)
 
                             Nothing ->
                                 Err (CannotGetA x)
@@ -176,58 +223,26 @@ makeChangesHelp changes getA getB ( x, y ) path =
             in
             case change of
                 Ok c ->
-                    makeChangesHelp (c :: changes) getA getB ( prevX, prevY ) tail
+                    makeChangesHelp areSimilar (c :: changes) getA getB ( prevX, prevY ) tail
 
                 Err e ->
                     Err e
 
 
--- Myers's O(ND) algorithm (http://www.xmailserver.org/diff2.pdf)
-
-
-ond : (Int -> Maybe a) -> (Int -> Maybe a) -> Int -> Int -> List ( Int, Int )
-ond getA getB m n =
+{-| Wu's O(NP) algorithm (<http://myerslab.mpi-cbg.de/wp-content/uploads/2014/06/np_diff.pdf>)
+-}
+onp : (a -> a -> Maybe similar) -> (Int -> Maybe a) -> (Int -> Maybe a) -> Int -> Int -> List ( Int, Int )
+onp areSimilar getA getB m n =
     let
-        v =
-            Array.initialize (m + n + 1) (always [])
-    in
-    ondLoopDK (snake getA getB) m 0 0 v
-
-
-ondLoopDK :
-    (Int -> Int -> List ( Int, Int ) -> ( List ( Int, Int ), Bool ))
-    -> Int
-    -> Int
-    -> Int
-    -> Array (List ( Int, Int ))
-    -> List ( Int, Int )
-ondLoopDK snake_ offset d k v =
-    if k > d then
-        ondLoopDK snake_ offset (d + 1) (-d - 1) v
-
-    else
-        case step snake_ offset k v of
-            Found path ->
-                path
-
-            Continue v_ ->
-                ondLoopDK snake_ offset d (k + 2) v_
-
-
-
--- Wu's O(NP) algorithm (http://myerslab.mpi-cbg.de/wp-content/uploads/2014/06/np_diff.pdf)
-
-
-onp : (Int -> Maybe a) -> (Int -> Maybe a) -> Int -> Int -> List ( Int, Int )
-onp getA getB m n =
-    let
+        v : Array (List ( Int, Int ))
         v =
             Array.initialize (m + n + 1) (always [])
 
+        delta : Int
         delta =
             n - m
     in
-    onpLoopP (snake getA getB) delta m 0 v
+    onpLoopP (snake areSimilar getA getB) delta m 0 v
 
 
 onpLoopP :
@@ -239,6 +254,7 @@ onpLoopP :
     -> List ( Int, Int )
 onpLoopP snake_ delta offset p v =
     let
+        ks : List Int
         ks =
             if delta > 0 then
                 List.reverse (List.range (delta + 1) (delta + p))
@@ -284,9 +300,11 @@ step :
     -> StepResult
 step snake_ offset k v =
     let
+        fromLeft : List ( Int, Int )
         fromLeft =
             Maybe.withDefault [] (Array.get (k - 1 + offset) v)
 
+        fromTop : List ( Int, Int )
         fromTop =
             Maybe.withDefault [] (Array.get (k + 1 + offset) v)
 
@@ -320,17 +338,18 @@ step snake_ offset k v =
 
 
 snake :
-    (Int -> Maybe a)
+    (a -> a -> Maybe similar)
+    -> (Int -> Maybe a)
     -> (Int -> Maybe a)
     -> Int
     -> Int
     -> List ( Int, Int )
     -> ( List ( Int, Int ), Bool )
-snake getA getB nextX nextY path =
+snake areSimilar getA getB nextX nextY path =
     case ( getA nextX, getB nextY ) of
         ( Just a, Just b ) ->
-            if a == b then
-                snake
+            if a == b || areSimilar a b /= Nothing then
+                snake areSimilar
                     getA
                     getB
                     (nextX + 1)
